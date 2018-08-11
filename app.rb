17a9fbec88203c2d1fecc1ae77f1bf02458515e9
Monica  Karasek <mlkarasek@gmail.com>
2018-08-11 18:48:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,9 +12,11 @@ class App < Sinatra::Base
     erb :reversed
   end
 
-  get '/friends' do
-    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
- 
+  <% @friends.each do |friend| %>
+    <h2><%= friend %></h2>
+    <% end %>
+    
+
   erb :friends
 
   end
